295ddd9a6ec485399eea2c3f8bda12ea8e408ad3
Antonio Oro Dungao <antonioorodungao@yahoo.com>
2020-03-28 12:19:40 +0300

    Updates

---
+++ precache-manifest.c5d6fb6047c214953de2727a230e47a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be51e70afcce56456a8a03aebe953df",
+    "url": "/reactgithubpage/index.html"
+  },
+  {
+    "revision": "aa398cce3dea8ae15211",
+    "url": "/reactgithubpage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9e3e0ee27979c2358d0c",
+    "url": "/reactgithubpage/static/js/2.7ed526b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactgithubpage/static/js/2.7ed526b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa398cce3dea8ae15211",
+    "url": "/reactgithubpage/static/js/main.906e7d2b.chunk.js"
+  },
+  {
+    "revision": "52f080e7ebe5de17a7a8",
+    "url": "/reactgithubpage/static/js/runtime-main.918e632a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactgithubpage/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff8df965fa06cd689711f897b8aa0cd6.js"
+  "/reactgithubpage/precache-manifest.c5d6fb6047c214953de2727a230e47a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactgithubpage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
